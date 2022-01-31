db11632bcfb8f41c2fc2de16d494225de2b478b4
Henrique Herbst Rosa <henriquehhr@gmail.com>
2022-01-31 15:18:44 -0300

    adicionado confirmar pedido e nome do cliente

--- script.js
+++ script.js
@@ -1,79 +1,100 @@
 let pratoSelecionado = null;
 let bebidaSelecionada  = null;
 let sobremesaSelecionada  = null;
-const botaoFecharPedido = document.querySelector("#botao-fechar-pedido");
-
+let linkWhatsapp;
 
 function selecionar(elementoCLicado){
     
-    let clicadoAnteriormente = elementoCLicado.classList.contains("selecionado");
-    
+    let clicadoAnteriormente = elementoCLicado.classList.contains("opcao--selecionada");
+    const botaoFecharPedido = document.querySelector("#botao-fechar-pedido");
+
     if (clicadoAnteriormente) {
-        if(elementoCLicado.classList.contains("dish")){
-            pratoSelecionado.classList.remove("selecionado");
+        if(elementoCLicado.classList.contains("js-prato")){
+            pratoSelecionado.classList.remove("opcao--selecionada");
             pratoSelecionado = null;
         }
-        else if(elementoCLicado.classList.contains("drink")){
-            bebidaSelecionada.classList.remove("selecionado");
+        else if(elementoCLicado.classList.contains("js-bebida")){
+            bebidaSelecionada.classList.remove("opcao--selecionada");
             bebidaSelecionada = null;
         }
-        else if(elementoCLicado.classList.contains("dessert")){
-            sobremesaSelecionada.classList.remove("selecionado");
+        else if(elementoCLicado.classList.contains("js-sobremesa")){
+            sobremesaSelecionada.classList.remove("opcao--selecionada");
             sobremesaSelecionada = null;
         }
-        if (botaoFecharPedido.classList.contains("fechar-pedido")){
-            botaoFecharPedido.classList.remove("fechar-pedido");
+        if (!botaoFecharPedido.disabled){
+            botaoFecharPedido.classList.remove("botao-fechar-pedido--habilitado");
             botaoFecharPedido.innerHTML = "Selecione os 3 itens<br> para fechar o pedido";
+            botaoFecharPedido.disabled = true;
         }
     }
-    else if(!clicadoAnteriormente){
-        if(elementoCLicado.classList.contains("dish")){
-            if(pratoSelecionado===null)
-                pratoSelecionado = elementoCLicado;
-            else{
-                pratoSelecionado.classList.remove("selecionado");
-                pratoSelecionado = elementoCLicado;
+    else if(!clicadoAnteriormente) {
+        if(elementoCLicado.classList.contains("js-prato")){
+            if (pratoSelecionado != null){
+                pratoSelecionado.classList.remove("opcao--selecionada");
             }
-            elementoCLicado.classList.add("selecionado");
+            pratoSelecionado = elementoCLicado;
         }
-        else if(elementoCLicado.classList.contains("drink")){
-            if(bebidaSelecionada===null)
-                bebidaSelecionada = elementoCLicado;
-            else{
-                bebidaSelecionada.classList.remove("selecionado");
-                bebidaSelecionada = elementoCLicado;
+        else if(elementoCLicado.classList.contains("js-bebida")){
+            if(bebidaSelecionada != null){
+                bebidaSelecionada.classList.remove("opcao--selecionada");    
             }
-            elementoCLicado.classList.add("selecionado");
+            bebidaSelecionada = elementoCLicado;
         }
-        else if(elementoCLicado.classList.contains("dessert")){
-            if(sobremesaSelecionada===null)
-                sobremesaSelecionada = elementoCLicado;
-            else{
-                sobremesaSelecionada.classList.remove("selecionado");
-                sobremesaSelecionada = elementoCLicado;
+        else if(elementoCLicado.classList.contains("js-sobremesa")){
+            if(sobremesaSelecionada != null){
+                sobremesaSelecionada.classList.remove("opcao--selecionada");
             }
-            elementoCLicado.classList.add("selecionado");
+            sobremesaSelecionada = elementoCLicado;
         }
+        elementoCLicado.classList.add("opcao--selecionada");
     }
 
     if(pratoSelecionado != null && bebidaSelecionada != null && sobremesaSelecionada != null){
-        botaoFecharPedido.classList.add("fechar-pedido");
-        
-
-        let nomePratoSelecionado = pratoSelecionado.querySelector("h3").innerText;
-        let nomeBebidaSelecionada = bebidaSelecionada.querySelector("h3").innerText;
-        let nomeSobremesaSelecionada = sobremesaSelecionada.querySelector("h3").innerText;
-
-        let precoPratoSelecionado = Number(pratoSelecionado.querySelector(".price").innerText.slice(3).replace(",","."));
-        let precoBebidaSelecionada = Number(bebidaSelecionada.querySelector(".price").innerText.slice(3).replace(",","."));
-        let precoSobremesaSelecionada = Number(sobremesaSelecionada.querySelector(".price").innerText.slice(3).replace(",","."));
-        let valorTotal = (precoPratoSelecionado + precoBebidaSelecionada + precoSobremesaSelecionada).toFixed(2);
-
-        let mensagem = 
-        encodeURIComponent(`Olá, gostaria de fazer o pedido:\n - Prato: ${nomePratoSelecionado}\n - Bebida: ${nomeBebidaSelecionada}\n - Sobremesa: ${nomeSobremesaSelecionada}\n Total: R$ ${valorTotal}`);
-        let linkWhatsapp = "https://wa.me/?text=" + mensagem;
-        botaoFecharPedido.innerHTML = `<a href="${linkWhatsapp}" target="_blank">Fechar pedido</a>`;
+        botaoFecharPedido.classList.add("botao-fechar-pedido--habilitado");
+        botaoFecharPedido.disabled = false;
+        botaoFecharPedido.innerHTML = "Fechar pedido";
     }
 
+}
 
+function fecharPedido(){
+    
+    nomeCLiente = prompt("Qual o seu nome?");
+    enderecoCLiente = prompt("Qual o seu endereço?");
+    document.querySelector(".tela-branca-opaca").classList.remove("desaparecer");
+    document.querySelector("body").classList.add("body__impedir-rolagem-vertical");
+
+    let nomePratoSelecionado = pratoSelecionado.querySelector("h3").innerText;
+    let nomeBebidaSelecionada = bebidaSelecionada.querySelector("h3").innerText;
+    let nomeSobremesaSelecionada = sobremesaSelecionada.querySelector("h3").innerText;
+
+    let precoPratoSelecionado = Number(pratoSelecionado.querySelector(".opcao__base__preco").innerText.slice(3).replace(",","."));
+    let precoBebidaSelecionada = Number(bebidaSelecionada.querySelector(".opcao__base__preco").innerText.slice(3).replace(",","."));
+    let precoSobremesaSelecionada = Number(sobremesaSelecionada.querySelector(".opcao__base__preco").innerText.slice(3).replace(",","."));
+    
+    let valorTotal = (precoPratoSelecionado + precoBebidaSelecionada + precoSobremesaSelecionada).toFixed(2);
+
+    document.querySelector(".confirmar-pedido__nome-prato").innerText = nomePratoSelecionado;
+    document.querySelector(".confirmar-pedido__nome-bebida").innerText = nomeBebidaSelecionada;
+    document.querySelector(".confirmar-pedido__nome-sobremesa").innerText = nomeSobremesaSelecionada;
+
+    document.querySelector(".confirmar-pedido__preco-prato").innerText = (precoPratoSelecionado.toFixed(2)).replace(".",",");
+    document.querySelector(".confirmar-pedido__preco-bebida").innerText = (precoBebidaSelecionada.toFixed(2)).replace(".",",");
+    document.querySelector(".confirmar-pedido__preco-sobremesa").innerText = (precoSobremesaSelecionada.toFixed(2)).replace(".",",");
+
+    document.querySelector(".confirmar-pedido__preco-total").innerText = "R$ " + valorTotal.replace(".",",");
+
+    let mensagem = 
+    encodeURIComponent(`Olá, gostaria de fazer o pedido:\n - Prato: ${nomePratoSelecionado}\n - Bebida: ${nomeBebidaSelecionada}\n - Sobremesa: ${nomeSobremesaSelecionada}\n Total: R$ ${valorTotal}`);
+    linkWhatsapp = "https://wa.me/5518997472691?text=" + mensagem + encodeURIComponent("\n\n Nome: " + nomeCLiente + "\n Endereço: " + enderecoCLiente);
+}
+
+function confirmarPedido() {
+    window.open(linkWhatsapp);
+    window.location.reload();
+}
+
+function cancelarPedido() {
+    document.querySelector(".tela-branca-opaca").classList.add("desaparecer");
+    document.querySelector("body").classList.remove("body__impedir-rolagem-vertical");
 }
